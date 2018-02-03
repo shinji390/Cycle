cc109b38d997991c0d132a2800574ff15fbeaa84
Junki Yamada <fogg.smiths@gmail.com>
2018-02-03 21:12:56 +0900

    create_destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,56 @@
 class CommentsController < ApplicationController
-  def edit
+  before_action :post_id_params, only: [ :create, :edit, :update, :destroy ]
+  before_action :comment_id_params, only: [ :edit, :update ]
+
+  def create
+    @comment = Comment.create(user_id: current_user.id,
+                              post_id: params[:post_id],
+                              text: params[:text],
+                              video: params[:video],
+                              url: params[:url],
+                              melody: params[:melody])
+    respond_to do |format|
+      format.html { redirect_to posts_path }
+      format.js
+    end
   end
+
+  def edit
+    respond_to do |format|
+      format.html
+      format.js { @comment_id = @comment.id }
+    end
+  end
+
+  def update
+    @comment.update(comment_params)
+    respond_to do |format|
+      format.html
+      format.js { @comment_id = @comment.id }
+    end
+  end
+
+  def destroy
+    comment = Comment.find_by(user_id: current_user.id, post_id: params[:post_id])
+    comment.destroy
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: posts_path)}
+      format.js
+    end
+  end
+
+private
+
+  def post_id_params
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text, :video, :url, :melody)
+  end
+
+  def comment_id_params
+    @comment = Comment.find(params[:id])
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :text, presence: true, length: { maximum: 1200 }
+  validates :url, format: { with: /https:\/\/www.youtube.com\/watch\?v=(.+)/ }, allow_blank: true
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 end
