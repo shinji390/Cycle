4657556f3a0a83fc6bdc5bf3ee203bcb739dc15e
Junki Yamada <fogg.smiths@gmail.com>
2018-02-18 15:17:57 +0900

    fadein_add

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -15,3 +15,17 @@ setTimeout(function(){
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
+
+// fadein
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
