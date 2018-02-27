5c2c0ac513c3d0d92fb7e0dfc25a0804d28902ef
Junki Yamada <fogg.smiths@gmail.com>
2018-02-27 21:12:36 +0900

    mailer_fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => ENV["DEVISE_EMAIL"],
+    :user_name => ENV["GMAIL"],
     :password => ENV["GMAIL_PW"],
     :authentication => :plain,
     :enable_starttls_auto => true
