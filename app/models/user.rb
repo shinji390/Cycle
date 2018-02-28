1d5c0de8be1b9483bbbbc186cc34c23fc0e09d51
Junki Yamada <fogg.smiths@gmail.com>
2018-02-28 12:19:25 +0900

    message_error_fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  has_many :messages, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
